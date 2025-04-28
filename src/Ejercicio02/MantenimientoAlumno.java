2d8fb7f3362b2284da1e222f8d06044b31d403af
Daryit03 <darvinzelada@gmail.com>
2025-04-27 23:14:48 -0500

    Cambie nombre del arreglo

--- src/Ejercicio02/MantenimientoAlumno.java
+++ src/Ejercicio02/MantenimientoAlumno.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 
 public class MantenimientoAlumno {
     
-    private ArrayList<Alumno> listaAlumnos;
+    private ArrayList<Alumno> ListaA;
     
     //constructor
     public MantenimientoAlumno(){
-        listaAlumnos = new ArrayList<>(); //objeto de la array list 
+        ListaA = new ArrayList<>(); //objeto de la array list 
     }
     
     //Metodos para manipular el arreglo
     public void AgregarAlumno (Alumno alumno){
-        listaAlumnos.add(alumno);
+        ListaA.add(alumno);
     }
     
     //Metodo que obtenga la lista de Alumnos
     public ArrayList<Alumno> ObtenerAlumno() {
-        return listaAlumnos;
+        return ListaA;
     }
 }
